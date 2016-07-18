d09d65a9561a6a26d79bb1960a078facbf381245
Yajun Fu <fuyajun1983cn@163.com>
2016-07-18 17:08:38 +0800

    add assoc retry mechanism(10 times)

--- wpa_supplicant/config_ssid.h
+++ wpa_supplicant/config_ssid.h
@@ -562,6 +562,14 @@ struct wpa_ssid {
 	 */
 	int export_keys;
 
+#ifdef FYJ_P2P_TEST
+	/**
+	 * assoc_retry - Number of times association should be retried.
+	 */
+	int assoc_retry;
+#endif
+
+
 #ifdef CONFIG_HT_OVERRIDES
 	/**
 	 * disable_ht - Disable HT (IEEE 802.11n) for this network
